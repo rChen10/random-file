95461b4f34a1530429078ab747788290218f1f3d
mcheng3 <michael.cheng65@gmail.com>
2017-10-29 19:40:41 -0400

    Add generate random #s

--- random.c
+++ random.c
@@ -5,14 +5,27 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-unsigned int generate(int fildes){
-  
+unsigned int generate(int filedes){
+	int *randos = (int  *) malloc(sizeof(int));
+	int output = read(filedes, randos, 4);
+	if (output < 0){
+		printf("Error: %s\n", strerror(errno));
+	}
+	else{
+		return *randos;
+	}
 }
 
 int main(){
-  int fldis = open("/dev/random", O_RDONLY);
-  int *randos = (int  *) malloc(sizeof(int) * 100);
-  read(fldis, randos, 100);
-  prinf("%d\n"
-  
+	int fldis = open("/dev/random", O_RDONLY);
+	printf("%d\n", generate(fldis));
+	printf("Generating random numbers:\n");
+	int n = 0;
+	int ary[10]; 
+	while(n < 10){
+		ary[n] = generate(fldis);
+		printf("	random %d: %d\n", n, ary[n]);
+		n++;
+	}
+
 }
