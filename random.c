6e1f469bc35a74029c330852c2cb07c4970d056e
Ricky Chen <rchen10@stuy.edu>
2017-10-30 01:42:10 +0000

    Finished first two parts. Remember to create the makefile

--- random.c
+++ random.c
@@ -5,14 +5,59 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-unsigned int generate(int fildes){
+int generate(int fldis){
+  //printf("opened\n");
+  //printf("%d\n", fldis);
+  //strerror(errno);
+  int randos;
+  read(fldis, &randos, sizeof(randos));
+  //printf("read\n");
+  printf("%d\n", randos);
+  return randos;
+}
+
+void write_to(int fldis, int *randos, int num){
+  if (! num) return;
+  write(fldis, randos, sizeof(*randos));
+  write(fldis, "\n", sizeof "\n");
+  write_to(fldis, ++randos, --num);
+}
+
+void read_from(int fldis){
   
 }
 
 int main(){
+  int nums[10];
+  int *ran = nums;
+  int i = 0;
   int fldis = open("/dev/random", O_RDONLY);
-  int *randos = (int  *) malloc(sizeof(int) * 100);
-  read(fldis, randos, 100);
-  prinf("%d\n"
+  if (fldis < 0){
+    printf("%s\n", strerror(errno));
+    return;
+  }
+  printf("Generating random numbers:\n");
+  for (; i<10; i++){
+    printf("random %d: ", i);
+    nums[i] = generate(fldis);
+  }
+  close(fldis);
+
   
+  printf("Writing numbers to random_output...\n");
+  fldis = open("random_output", O_CREAT | O_WRONLY, 644);
+  if (fldis < 0){
+    printf("%s\n", strerror(errno));
+    return;
+  }
+  write_to(fldis, ran, sizeof nums / sizeof(int));
+  //printf("%s\n", strerror(errno));
+  //printf("%d\n", fldis);
+  close(fldis);
+
+  
+  printf("Reading numbers from random_output...\n");
+  fldis = open("random_output", O_RDONLY);
+  
+  close(fldis);
 }
